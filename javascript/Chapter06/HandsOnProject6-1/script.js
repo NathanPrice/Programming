5d94daed0c797a07fe6e34ed1811395d291ce5b5
imNP <nathan.a.price@redlandscc.edu>
2015-04-09 09:50:56 -0500

    Finished project

--- javascript/Chapter06/HandsOnProject6-1/script.js
+++ javascript/Chapter06/HandsOnProject6-1/script.js
@@ -7,56 +7,58 @@
  *
  *    Filename: script.js
  */
+
  
  "use strict";
  
  var formValidity = true;
  
  /* validate required fields */
- var inputElements = document.querySelectorAll("#contactinfo input");
- var errorDiv = document.getElementById("errorText");
- var elementCount = inputElements.length;
- var requiredValidity = true;
- var currentElement;
- 
- try {
-	 for (var i = 0; i < elementCount; i++) {
-		 // validate all input elements in fieldset
-		 currrentElement = inputElements[i];
-		 if (currentElement.value === "") {
-			 currentElement.style.background = "rgb(255,233,233)";
-			 requiredValidity = false;
-		 }
-		 else {
-			 currentElement.style.background = "white"
-		 }
-	 }
-	 if (requiredValidity === false) {
-		 throw "Please complete all fields."
-	 }
-	 errorDiv.style.display = "none";
-	 errorDiv.innerHTML = "";
+ function validateRequired() {
+ 	var inputElements = document.querySelectorAll("#contactinfo input");
+ 	var errorDiv = document.getElementById("errorText");
+ 	var elementCount = inputElements.length;
+ 	var requiredValidity = true;
+ 	var currentElement;
+
+ 	try {
+ 		for (var i = 0; i < elementCount; i++) {
+ 			// validate all input elements in fieldset
+ 			currentElement = inputElements[i];
+ 			if (currentElement.value === "") {
+ 				currentElement.style.background = "rgb(255,233,233)";
+ 				requiredValidity = false;
+ 			}
+ 			else {
+ 				currentElement.style.background = "white";
+ 			}
+ 		}
+ 		if (requiredValidity === false) {
+ 			throw "Please complete all fields.";
+ 		}
+ 		errorDiv.style.display = "none";
+ 		errorDiv.innerHTML = "";
+ 	}
+ 	catch(msg) {
+ 		errorDiv.style.display = "block";
+ 		errorDiv.innerHTML = msg;
+ 		formValidity = false;
+ 	}
  }
- 
- catch(msg) {
-	 errorDiv.style.display = "block";
-	 errorDiv.innerHTML = msg;
-	 formValidity = false;
- }
- 
- /* create event listeners */
- function createEventListeners() {
-	 var form = document.getElementsByTagName("form")[0];
-	 if (form.addEventListener) {
-		 form.addEventListener("submit", validateForm, false);
-	 }
-	 else if (form.attachEvent) {
-		 form.attatchEvent("onsubmit", validateForm);
-	 }
- }
- 
- /* validate form */
- function validateForm(evt) {
+
+/* create event listeners */
+function createEventListeners() {
+	var form = document.getElementsByTagName("form")[0];
+	if (form.addEventListener) {
+		form.addEventListener("submit", validateForm, false);
+	}
+	else if (form.attachEvent) {
+		form.attachEvent("onsubmit", validateForm);
+	}
+}
+
+/* validate form */
+function validateForm(evt) {
 	if (evt.preventDefault) {
 		evt.preventDefault(); // prevent form from submitting
 	}
@@ -64,17 +66,16 @@
 		evt.returnValue = false; // prevent form from submitting in IE8
 	}
 	formValidity = true; // reset value for revalidation
-	validateRequired();
+	validateRequired(); // reset value for revalidation
 	if (formValidity === true) {
 		document.getElementsByTagName("form")[0].submit();
 	}
- }
- 
- /* run setup functions when page finishes loading */
- if (window.addEventListener) {
-	 window.addEventListener("load", createEventListeners, false);
- }
- else if (window.attachEvent) {
-	 window.attachEvent("onload", createEventListeners);
- }
- 
+}
+
+/* run setup functions when page finished loading */
+if (window.addEventListener) {
+	window.addEventListener("load", createEventListeners, false);
+}
+else if (window.attachEvent) {
+	window.attachEvent("onload", createEventListeners);
+}
